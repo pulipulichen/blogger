bb4387fea086e5f01b9139f04deda43211f87fb6
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-07-13 21:28:34 +0800

    更新OpenVZ轉換：加上訊息提示

--- project/20130713-openvz-template/openvz_CT_to_template.sh
+++ project/20130713-openvz-template/openvz_CT_to_template.sh
@@ -6,78 +6,92 @@
 # 2013/07/13    Pulipuli Chen   First release
 
 # 歡迎訊息
-echo "================================"
-echo "  OpenVZ Container to Template"
-echo "================================"
+echo "========================================="
+echo "  OpenVZ Container to Template Packager"
+echo "========================================="
+
+# 宣告目錄參數
+container_dir=/var/lib/vz/private/
+template_dir=/var/lib/vz/template/cache/
+if [ ! -d $container_dir ] || [ ! -d $template_dir ]; then
+    echo "This script only for Proxmox VE 1~3"
+    echo "http://www.proxmox.com/downloads/category/iso-images-pve"
+    echo "Abort"
+    exit 0
+fi
 
 # 請輸入要轉換的VMID
-read -p "Please enter OpenVZ Container's VMID: [100] " vmid
+read -p "Please enter OpenVZ container's VMID: [100] " vmid
 if [ -z $vmid ]; then
       vmid=100
 fi
 
 # 回傳訊息，告知使用者要轉換的VMID
-echo "You want to convert Container VMID $vmid"
+echo "You want to package container VMID $vmid to template"
 
-# 宣告目錄參數
-container_dir=/var/lib/vz/private/
+# 宣告虛擬機器的目錄
 ct_dir=$container_dir/$vmid
-template_dir=/var/lib/vz/template/cache/
 
 # 檢查該VMID的虛擬機器是否存在
 if [ -d $ct_dir ]; then
 
     # 如果存在的話
     
-    read -p "Please enter template OS (ex: centos-5, debain-6.0, ubuntu-10.04): [centos-5] " template_os
+    # 詢問作業系統與其版本
+    read -p "Please enter template's OS and version (ex: centos-5, debain-6.0, ubuntu-10.04): [centos-5] " template_os
     if [ -z $template_os ]; then
       template_os=centos-5
     fi
     until [[ "$template_os" == *"-"* ]]; do
-        echo "Template OS should include '-', ex: centos-5, debain-6.0, ubuntu-10.04. "
-        read -p "Please enter template OS again : [centos-5]" template_os
+        echo "Template's OS and version should include '-', ex: centos-5, debain-6.0, ubuntu-10.04. "
+        read -p "Please enter template's OS and version again : [centos-5]" template_os
         if [ -z $template_os ]; then
           template_os=centos-5
         fi
     done
 
-    read -p "Please enter template name (ex: standard, moodle, dspace-dlll): [custom] " name
+    # 詢問應用系統的名字
+    read -p "Please enter template's application name (ex: standard, moodle, dspace-dlll): [custom] " name
     if [ -z $name ]; then
       name=custom
     fi
     until [[ "$name" != *"_"* ]]; do
-        echo "Template name should NOT include '_', ex: standard, moodle, dspace-dlll."
-        read -p "Please enter template name again : [custom] " name
+        echo "Template's application name should NOT include '_', ex: standard, moodle, dspace-dlll."
+        read -p "Please enter template's application name again : [custom] " name
         if [ -z $name ]; then
           name=custom
         fi
     done
 
-    read -p "Please enter template version (ex: 5.6-1, 10.04-4): [1.0-0] " version
+    # 詢問應用系統的版本號
+    read -p "Please enter template's application version (ex: 5.6-1, 10.04-4): [1.0-0] " version
     if [ -z $version ]; then
       version=1.0-0
     fi
     until [[ "$version" == *"."*"-"* ]]; do
-        echo "Template version should include '.' and '-', ex: 5.6-1, 10.04-4."
-        read -p "Please enter template version again : [1.0-0] " version
+        echo "Template's application version should include '.' and '-', ex: 5.6-1, 10.04-4."
+        read -p "Please enter template's application version again : [1.0-0] " version
         if [ -z $version ]; then
           version=1.0-0
         fi
     done
 
-    read -p "Please enter template arch. (ex: i386): [i386] " template_arch
+    # 詢問樣板的架構
+    read -p "Please enter template's architecture (arch.) (ex: i386): [i386] " template_arch
     if [ -z $template_arch ]; then
       template_arch=i386
     fi
 
     template_tar=${template_os}-${name}_${version}_${template_arch}.tar.gz
 
-    read -p "Do you wish to convert VMID $vmid to $template_tar? [Y/n] " RESP
+    # 確認轉換項目
+    read -p "Do you wish to package container VMID $vmid to template $template_tar? [Y/n] " RESP
     if [ "$RESP" = "n" ]; then
       echo "Abort"
       exit 0
     fi
 
+    # 檢查是否有同樣名稱的樣板
     template_path=${template_dir}${template_tar}
     if [ -d $template_path ]; then
         read -p "${template_tar} existed. Do you wish to overwrite it? [Y/n] " overwrite
@@ -87,14 +101,22 @@ if [ -d $ct_dir ]; then
         fi
     fi
 
+    # 停止運作中的虛擬機器
     echo "Stop VMID $vmid..."
     vzctl stop $vmid
 
-    echo "Package VMID $vmid to template..."
-    # /var/lib/vz/template/cache/centos-5-standard_5.6-1-dspace-dlll.tar.gz
+    # 開始進行轉換
+    echo "Start to package VMID $vmid to template $template_tar ..."
     cd $ct_dir
     tar -czvf --overwrite ${template_path} ./ 
+
+    # 完成訊息
+    echo "Package complete!"
+    echo "You can use template $template_tar in your Proxmox VE now."
+    echo "Template location path is $template_path"
 else
+
+    # 如果沒有該虛擬機器，則停止腳本檔
     echo "VMID $vmid not exist"
     echo "Abort"
 fi

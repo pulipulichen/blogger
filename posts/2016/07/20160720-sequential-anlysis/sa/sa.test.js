a7b810720483e22260c9d70b7b21159f4b0a81e7
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-07-23 09:56:14 +0800

    0723 同時進行JavaScript版本的改造

---
+++ posts/2016/07/20160720-sequential-anlysis/sa/sa.test.js
@@ -0,0 +1,10 @@
+
+// ------------------------------------
+
+$(function () {
+    var _obs = "PG PG PG PG PG PG PG PG PG PG GP GP GP GP GP GSGSGSGSGSGSGSGSGSGSGSGSGSGSGSPSPSPSPSPSPSPSPSPSPSPSPSPSPSPSPUPUPUPUSUTSUTSUTSUTSUTSUTSUTSUTSUTSUTSUTSUTSUTSUTSUT";
+    $(".obs.textarea").val(_obs);
+    $("[name='exp_model'][value='first_order']").click();
+    sa_start();
+    
+});

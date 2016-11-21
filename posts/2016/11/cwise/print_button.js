3af0ea5841210652c2fd279ca1a0aa8cd183076e
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-11-21 16:28:18 +0800

    1121 cwise: rangy-annotation & print_button

--- posts/2016/11/cwise/print_button.js
+++ posts/2016/11/cwise/print_button.js
@@ -28,7 +28,8 @@ var _load_jquery_print_button = function () {
 var _setup_print_button = function () {
     $(function () {
         //$("body").append('');
-        $("body").append('<div style="text-align: center;position: fixed;bottom: 0;width: 100%;background-color: white;padding-bottom: 5px;"><hr /><button style="box-shadow: 0 0 0 0 rgba(34,36,38,.15) inset; background-color: #975b33; color: #fff; text-shadow: none; background-image: none; font-size: 1rem !important; padding: .78571429em 1.5em; min-height: 1em; height: auto; border-radius: .28571429rem; display: inline-block;" onclick="window.print();" type="button">列印</button></div>');
+        $("head").append('<link rel="stylesheet" href="//pulipulichen.github.io/blogger/posts/2016/11/cwise/print_button.css">');
+        $("body").append('<div class="print-button-container" style="text-align: center;position: fixed;bottom: 0;width: 100%;background-color: white;padding-bottom: 5px;"><hr /><button style="box-shadow: 0 0 0 0 rgba(34,36,38,.15) inset; background-color: #975b33; color: #fff; text-shadow: none; background-image: none; font-size: 1rem !important; padding: .78571429em 1.5em; min-height: 1em; height: auto; border-radius: .28571429rem; display: inline-block;" onclick="window.print();" type="button">列印</button></div>');
     });
 };
 

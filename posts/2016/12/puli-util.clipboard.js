8de40b6c8efe91f8e43526bec6d68580bb62f318
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-12-17 18:46:22 +0800

    1217 puli-util.clipboard.js

---
+++ posts/2016/12/puli-util.clipboard.js
@@ -0,0 +1,45 @@
+if (typeof(PULI_UTIL) === "undefined") {
+    PULI_UTIL = {};
+}
+
+PULI_UTIL.clipboard = {
+    init: function (_callback) {
+        //console.log(typeof(Clipboard));
+        if (typeof(Clipboard) !== "function") {
+            var _clipboard_url = '//pulipulichen.github.io/blogger/posts/2016/12/clipboard.min.js';
+            if ($('head script[src="' + _clipboard_url + '"]').length === 0) {
+                $('head').append('<script type="text/javascript" src="' + _clipboard_url + '"></sc' + 'ript>');
+            }
+            $(function () {
+                setTimeout(function () {
+                    PULI_UTIL.clipboard.init(_callback);
+                }, 100);
+            });
+        } 
+        else {
+            if ($('#clipboard_button').length === 0) {
+                var _button = $('<button type="button" id="clipboard_button"></button>')
+                    //.attr("data-clipboard-text", _content)
+                    .hide()
+                    .appendTo("body");
+                    
+                var clipboard = new Clipboard('#clipboard_button');
+            }
+            $(function () {
+                _callback();
+            });
+        }
+    },
+    copy: function (_content) {
+        var _button = $('#clipboard_button')
+            .attr("data-clipboard-text", _content);
+        
+        _button.click();
+        //console.log([typeof(Clipboard), _button.data('clipboard-text')]);
+        //console.log("finish");
+    }
+}
+
+$(function () {
+    PULI_UTIL.clipboard.init();
+});

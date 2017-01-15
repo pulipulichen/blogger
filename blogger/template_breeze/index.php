9c387124edd3ff056996dd53f396c18fa8edf1fb
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-01-15 16:41:47 +0800

    0115 更換AddThis

--- blogger/template_breeze/index.php
+++ blogger/template_breeze/index.php
@@ -935,6 +935,7 @@
 
 <!-- JAVASCRIPT -->
 <!-- <script src="//load.sumome.com/" data-sumo-site-id="91b6a5a67cc23845f0765da427a8ac1c0b918c4e9226f12ea32abf1e8110df0c" async="async"></script> -->
+<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=pulipuli" async="async"></script> 
 <script type="text/javascript" src="//pulipulichen.github.io/blogger/blogger/js/google-analytics.js"></script>
 
 <b:if cond='data:blog.pageType == &quot;item&quot;'>

5ff87bbc59db18f4ce3e7ed9dd243694d5d5d546
pudding <pudding@pudding-dlll>
2017-03-09 12:51:17 +0800

    0309 留言轉連結

--- blogger/js/script-item.js
+++ blogger/js/script-item.js
@@ -384,3 +384,34 @@ $(function () {
     };
     _setup_iframe_anchor();
 });
+
+// ------------------------------
+// 20170309 留言網址變連結
+$(function () {
+    var _setup_comment_to_link = function () {
+        var _len = $("#comment-holder").length;
+        if (_len === 0) {
+            setTimeout(_setup_comment_to_link, 1000);
+        }
+        else {
+            $("#comment-holder .comment-content").html(function(i, inputText) {
+                var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+                //URLs starting with http://, https://, or ftp://
+                replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+                replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+                //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+                replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+                replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+                //Change email addresses to mailto:: links.
+                replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+                replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+                return replacedText;
+           });
+        }
+    };
+    _setup_comment_to_link();
+});

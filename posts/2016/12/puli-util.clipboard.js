fcc0e96661e24864c92488e40a83963ef123365d
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-09-27 22:07:12 +0800

    0929 改善被clipboard卡住的問題

--- posts/2016/12/puli-util.clipboard.js
+++ posts/2016/12/puli-util.clipboard.js
@@ -1,11 +1,11 @@
-if (typeof(PULI_UTIL) === "undefined") {
+if (typeof (PULI_UTIL) === "undefined") {
     PULI_UTIL = {};
 }
 
 PULI_UTIL.clipboard = {
     init: function (_callback) {
         //console.log(typeof(Clipboard));
-        if (typeof(Clipboard) !== "function") {
+        if (typeof (Clipboard) !== "function") {
             var _clipboard_url = '//pulipulichen.github.io/blogger/posts/2016/12/clipboard.min.js';
             if ($('head script[src="' + _clipboard_url + '"]').length === 0) {
                 $('head').append('<script type="text/javascript" src="' + _clipboard_url + '"></sc' + 'ript>');
@@ -15,27 +15,31 @@ PULI_UTIL.clipboard = {
                     PULI_UTIL.clipboard.init(_callback);
                 }, 100);
             });
-        } 
-        else {
+        } else {
             if ($('#clipboard_button').length === 0) {
                 var _button = $('<button type="button" id="clipboard_button"></button>')
-                    //.attr("data-clipboard-text", _content)
-                    .hide()
-                    .appendTo("body");
+                        //.attr("data-clipboard-text", _content)
+                        .hide()
+                        .appendTo("body");
+
+                try {
+                    var clipboard = new Clipboard('#clipboard_button');
+                }
+                catch (e) {
                     
-                var clipboard = new Clipboard('#clipboard_button');
+                }
             }
             $(function () {
-				if (typeof(_callback) ==="function") {
-					_callback();
-				}
+                if (typeof (_callback) === "function") {
+                    _callback();
+                }
             });
         }
     },
     copy: function (_content) {
         var _button = $('#clipboard_button')
-            .attr("data-clipboard-text", _content);
-        
+                .attr("data-clipboard-text", _content);
+
         _button.click();
         //console.log([typeof(Clipboard), _button.data('clipboard-text')]);
         //console.log("finish");

4f0783e99c7cd8f1f08273fa79c7de7ea804c5c4
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-12-26 01:11:15 +0800

    1226 調整FB粉專iframe

--- blogger/js/script.js
+++ blogger/js/script.js
@@ -575,5 +575,5 @@ $(function () {
 });
 
 var _load_fan_page = function () {
-  $('.widget.HTML.fb-fan-page .widget-content').html('<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpulipuli.blogspot&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=391880581194257" width="350" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>');
+  $('.widget.HTML.fb-fan-page .widget-content').html('<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpulipuli.blogspot&tabs=timeline&width=350&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=391880581194257" width="350" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>');
 }
